22d2b09357ffb7dd0d21eb8a9aff3625d4978155
Anna Godin <annagodin@Annas-MBP.home>
2020-04-17 22:55:43 -0400

    pushing feedback to the database done

--- code-clone/src/app/components/code-analysis/input-feedback-data/input-feedback-data.component.ts
+++ code-clone/src/app/components/code-analysis/input-feedback-data/input-feedback-data.component.ts
@@ -2,6 +2,9 @@ import {Component, Input, OnInit} from '@angular/core';
 import {CloneResults} from "../../../shared/models/CloneResults";
 import {CloneFeedback} from "../../../shared/models/CloneFeedback";
 import {CloneData} from "../../../shared/models/CloneData";
+import {DatabaseService} from "../../../shared/services/database/database.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {CloneFeedbackInterface} from "../../view-feedback-data/view-feedback-data.component";
 
 @Component({
   selector: 'app-input-feedback-data',
@@ -12,15 +15,40 @@ export class InputFeedbackDataComponent implements OnInit {
   @Input() cloneResults: CloneResults;
   // @Input() feedbackData: CloneFeedback[];
 
+  isFeedbackUploaded = false;
   step = 0;
+  cloneFeedbackList: CloneFeedback[];
 
-
-  constructor() {
+  constructor(private databaseService: DatabaseService) {
   }
 
   ngOnInit(): void {
+    this.databaseService.getData().subscribe(data => {
+      this.cloneFeedbackList = this.convertData(data);
+      this.printData();
+    });
   }
 
+  convertData(data) {
+    let feedbackData: CloneFeedback[] = [];
+    for (const i of data) {
+      let fb = new CloneFeedback(i["rating"], i["cloneType"], i["textFeedback"]);
+      fb.id = i["id"];
+      feedbackData.push(fb);
+      // let row = {cloneType: fb.cloneType, rating: fb.rating, textFeedback: fb.textFeedback, id: fb.id };
+    }
+    return feedbackData;
+  }
+
+
+  addFeedbackDataPoint(cloneFeedback: CloneFeedback) {
+    cloneFeedback = this.databaseService.createDataPoint(cloneFeedback);
+  }
+
+  printData() {
+    console.log("---------- here's your data");
+    console.log(this.cloneFeedbackList);
+  }
 
   setStep(index: number) {
     this.step = index;
@@ -35,10 +63,17 @@ export class InputFeedbackDataComponent implements OnInit {
   }
 
   uploadFeedback() {
-    console.log("~~~~~~~PRINTING FEEDBACK~~~~~~~");
-    for (let i=0; i<this.cloneResults.results.length; i++){
-      console.log(this.cloneResults.results[i])
+    // console.log("###### PRINTING FEEDBACK~~~~~~~");
+    for (let i = 0; i < this.cloneResults.results.length; i++) {
+      let feedbackDataPoint = this.cloneResults.results[i].feedback;
+      if (feedbackDataPoint.textFeedback == undefined) {
+        feedbackDataPoint.textFeedback = null;
+      }
+      this.addFeedbackDataPoint(feedbackDataPoint);
     }
+    this.isFeedbackUploaded = true;
+    // console.log("~~~~~~~~~~UPLOADED FEEDBACK~~~~~~~");
+
 
   }
 }
