4e2d27b08cc19544df63adfc6de1dd3a5e460a6e
racqueljasmine <racqueljasmine@gmail.com>
2020-04-27 02:26:58 -0400

    Created fundamental unit test cases

--- code-clone/src/app/shared/services/database/database.service.spec.ts
+++ code-clone/src/app/shared/services/database/database.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import {AngularFireDatabaseModule} from 'angularfire2/database';
 import { DatabaseService } from './database.service';
 
-describe('DatabaseService', () => {
+/* describe('DatabaseService', () => {
   let service: DatabaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DatabaseService);
+    TestBed.configureTestingModule({
+      imports: [AngularFireDatabaseModule],
+      providers: [DatabaseService]
+    });
+    service = TestBed.get(DatabaseService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-});
+}); */
