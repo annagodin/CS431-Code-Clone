5b91150ca28f3f76d393522758f82ce0f5bc8fcc
racqueljasmine <racqueljasmine@gmail.com>
2020-04-29 19:25:35 -0400

    Added more unit and intregative testing

--- code-clone/src/app/components/code-analysis/upload-inputs/upload-inputs.component.spec.ts
+++ code-clone/src/app/components/code-analysis/upload-inputs/upload-inputs.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By} from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { UploadInputsComponent } from './upload-inputs.component';
 
 describe('UploadInputsComponent', () => {
   let component: UploadInputsComponent;
   let fixture: ComponentFixture<UploadInputsComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,10 +21,35 @@ describe('UploadInputsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UploadInputsComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+/*
+  it(`should have value SNIPPET`, async(() => {
+    fixture = TestBed.createComponent(UploadInputsComponent);
+
+    const expected_value = InputType.SNIPPET;
+    component.referenceCodeType=expected_value;
+    expect(component.referenceCodeType).toEqual(expected_value);
+  }));
+*/
+
+  it('should have an `button` tag of `Get Results`', () => {
+    expect(de.query(By.css('button')).nativeElement.innerText).toBe('Get Results');
+  });
+
+  it('should click button `Get Results`', async(() => {
+    spyOn(component, 'goToResults');
+  
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+  
+    fixture.whenStable().then(() => {
+      expect(component.goToResults).toHaveBeenCalled();
+    });
+  }));
 });
